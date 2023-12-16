99aaad4e8f341b9fdefcc2a8b4175ad02f669372
Larissa L. Oliveira <worklarissaoliveira@gmail.com>
2023-12-16 15:36:05 -0300

    inicio dos códigos

---
+++ script.js
@@ -0,0 +1,10 @@
+/* M = c (1 + i)^t*/
+
+/*let capital = armazena o valor do capital inicial*/
+/*let juros = armazena o valor juros */
+/*let tempo = armazena a quantidade de meses que o investimento vai durar*/
+
+/* function formula (capital, juros) {
+    let conta = capital * (1 + juros)** tempo
+    console.log () 
+}*/
